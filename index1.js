3749d3c0390d64b72f1b468b5047270cd7d4e251
Bari Shavershian <bshavershyan@gmail.com>
2022-10-27 23:22:51 +0300

    HW_3

---
+++ index1.js
@@ -0,0 +1,4 @@
+var userName1=prompt("What is your name?")
+var userName2=prompt("how old are you?")
+var userName3=prompt("What city are you coming from?")
+alert(`${userName1} ` + `${userName2} ` + `${userName3} `)
