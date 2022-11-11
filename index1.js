b274a661786368b04d3328a383bd6e1b486ff25f
Bari Shavershian <bshavershyan@gmail.com>
2022-11-11 20:43:23 +0200

    HW_12

--- index1.js
+++ index1.js
@@ -1,6 +1,19 @@
-a=prompt("enter any number"); 
-b=prompt("enter any number"); 
-c=prompt("enter any number"); 
-if((b<a && a<c) || (c<a && a<b)) alert(a);
-if((a<b && b<c) || (c<b && b<a)) alert(b);
-if((a<c && c<b) || (b<c && c<a)) alert(c);
+// Створити масив, довжину та елементи якого задає користувач.
+
+// Відсортувати масив за зростанням.
+
+// Видалити елементи з масиву з 2 по 4 (включно!).
+
+// У міру змін виводити вміст масиву на сторінку.
+    let arrLength = Number (prompt ("Enter any number!"))
+    let arr = []; 
+    for(let i=0; i < arrLength; i++) {
+        while(
+          typeof (arr[i] = prompt('Enter any data (numbers, words...) №'+i)) != 'string' || arr[i].length ==0
+        );
+    }
+    arr.sort();
+    console.log(arr);
+    arr.splice(2,3)
+    console.log(arr);
+
